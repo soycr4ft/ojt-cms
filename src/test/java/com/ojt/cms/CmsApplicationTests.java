fabdca4e98de1f7039a13d56d4e9a8463b5557cd
soycr4ft <s0ycr4ft@gmail.com>
2025-09-18 17:35:33 +0900

    ojt cms project

---
+++ src/test/java/com/ojt/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojt.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
